5de7eaec41c0c3f80e505efcc0aa67c6292a4da0
김진홍 <deepred.kim@gmail.com>
2021-06-03 22:44:55 +0900

    axios 삽질 저장

--- src/server/controllers/apiController.js
+++ src/server/controllers/apiController.js
@@ -1,21 +1,43 @@
+const { ContactSupportOutlined } = require("@material-ui/icons");
 const axios = require("axios");
 const os = require("os");
 
-const getUsername = (req, res) =>
+const getUsername = (req, res) => {
   res.send({ username: os.userInfo().username });
+  return;
+};
 
-const getProfile = async (baekjoonId) => {
-  // axios
-  //   .get(`https://solved.ac/profile/${baekjoonId}/history`)
-  //   .then((response) => {
-  //     // console.log(response);
-  //   });
+const getProfile = (baekjoonId) => {
+  const acmicpc = fetchLastSubmitted(baekjoonId);
 
-  let response = await axios.get(
+  let lastSubmitted;
+  acmicpc.then((response) => {
+    console.log(response);
+    lastSubmitted = response;
+  });
+
+  return { lastSubmitted };
+};
+
+const fetchLastSubmitted = async (baekjoonId) => {
+  const response = await axios.get(
     `https://www.acmicpc.net/status?problem_id=&user_id=${baekjoonId}&language_id=-1&result_id=-1`
   );
 
   return response.data.split(`data-method="from-now">`)[1].split(`<`)[0];
+};
+
+const fetchTierExperience = (baekjoonId) => {
+  /// 티어
+  // <b> [1]
+  // </b> [0]
+  /// 누적 경험치
+  // <b> [2]
+  // </b> [0]
+  /// 경험치 데이터
+  // "expHistory":{"success":true,"result": [1]
+  // },"solvedHistory [0]
+  // 7일
 };
 
 exports.getUsername = getUsername;
