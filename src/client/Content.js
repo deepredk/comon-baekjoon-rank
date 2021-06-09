e1c0e3e0a078e369ddd7bdec9c5f8fc4648fc1d7
Deepred <deepred.kim@gmail.com>
2021-06-09 13:23:14 +0900

    Content.js 스타일컴포넌트화

--- src/client/Content.js
+++ src/client/Content.js
@@ -1,12 +1,17 @@
 import { Container } from '@material-ui/core';
 import React from 'react';
 import Rank from './Rank';
-import './Content.css';
+import { styled } from "@material-ui/core/styles";
+
+const StyledContainer = styled(Container)({
+  marginTop: "10px",
+  padding: "0",
+});
 
 export default function Content() {
   return (
-    <Container maxWidth="md" className="container">
+    <StyledContainer maxWidth="md" className="container">
       <Rank />
-    </Container>
+    </StyledContainer>
   );
 }
