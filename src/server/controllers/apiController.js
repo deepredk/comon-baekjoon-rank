bf4fcf78921351be99500b1c96f4b254d6b614de
Deepred <deepred.kim@gmail.com>
2021-06-09 15:21:26 +0900

    Update apiController.js

--- src/server/controllers/apiController.js
+++ src/server/controllers/apiController.js
@@ -1,24 +1,23 @@
-const { getProfile, getPeople, getTeams } = require("./crawler");
+import { getProfile, getPeople, getTeams } from "./crawler";
 
 const crawled = (people, members) => {
   this.people = people;
   this.member = members;
-}
+};
 
 // 개인 랭킹 json
 const getPersonalRank = (req, res) => {
   return res.json(getPeople());
-}
+};
 // 팀 랭킹 json
 const getTeamRank = (req, res) => {
   return res.json(getTeams());
-}
+};
 
 const getProfileInfo = (req, res) => {
   const { baekjoonId } = req.params;
-  getProfile(baekjoonId)
-    .then((profile) => res.send(profile));
-}
+  getProfile(baekjoonId).then((profile) => res.send(profile));
+};
 
 exports.getPersonalRank = getPersonalRank;
 exports.getTeamRank = getTeamRank;
